43eed7157ed7927660ffe78833cd992702d63786
Bunyod <shbunyod2197@gmail.com>
2026-02-18 16:49:06 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# 1
+from idlelib.editor import keynames
+
+student = {"Ali": 85,
+"Vali": 90,
+"Hasan": 78}
+print(len(keynames))
+# 2
+student = {"Ali": 85,
+           "Vali": 90,
+           "Hasan": 78}
+print(max(student.values()))
+# 3
+prices = {"apple": 5000, "banana": 3000, "cherry": 7000}
+print(sum(prices))
+# 4
+car = {"brand": "Chevrolet",
+       "year": 2022,
+       "color": "black"}
+print(car)
+# 5
+person = {"name": "Ali",
+          "age": 25,
+          "city": "Toshkent"}
+del person["age"]
+print(person)
